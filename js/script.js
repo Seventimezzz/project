6033c7033be94d0df10f7b710c7d2dc47759c748
Daniil <seventimezzz@gmail.com>
2022-06-30 19:20:40 +0300

    final_script

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false, 
-}
+};
+
+const a = prompt("Один из последний просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?"),
+      c = prompt("Один из последний просмотренных фильмов?", ""),
+      d = prompt("Один из последний просмотренных фильмов?", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
