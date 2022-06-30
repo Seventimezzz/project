e3677bdc02b111734d245e1fe35118604211a6f6
Daniil <seventimezzz@gmail.com>
2022-06-30 18:53:57 +0300

    script_two

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 const numberOfFilmes = +prompt('Сколько фильмов вы уже посмотрели?', "1")
+
+const personalMovieDB = {
+    count: numberOfFilmes,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false, 
+}
